301cc2fa3c367b559781a257c1aea7968a710882
Diohis <dimakulakov04mail.ru@gmail.com>
2024-02-07 18:33:43 +0700

    Block Dima
    Courier Customer

--- core/keyboards/inline.py
+++ core/keyboards/inline.py
@@ -1,6 +1,8 @@
-from core.google_doc.googleSheets import get_id_admin
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+import datetime
 
 start_button = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text="Посмотреть отзыв", callback_data="courier")],
@@ -8,22 +10,20 @@ start_button = InlineKeyboardMarkup(inline_keyboard=[
 ])
 
 
-def create_start_buttons(user_id: int) -> InlineKeyboardBuilder:
+
+def create_start_buttons()->InlineKeyboardBuilder:
     builder = InlineKeyboardBuilder()
     builder.row(InlineKeyboardButton(
         text="Заказчик",
-        callback_data="customer")
+        callback_data=f"customer")
     )
     builder.add(InlineKeyboardButton(
         text="Курьер",
-        callback_data="courier")
+        callback_data=f"courier")
     )
-    if user_id in get_id_admin():
-        builder.add(InlineKeyboardButton(text='Администратору', callback_data="admin"))
     return builder
 
-
-def create_courier_buttons(registration: bool, link=None) -> InlineKeyboardBuilder:
+def create_courier_buttons(registration: bool,link=None)->InlineKeyboardBuilder:
     builder = InlineKeyboardBuilder()
     builder.row(InlineKeyboardButton(
         text="Назад",
@@ -46,8 +46,7 @@ def create_courier_buttons(registration: bool, link=None) -> InlineKeyboardBuild
         ) 
     return builder
 
-
-def create_customer_buttons(registration: bool, link=None) -> InlineKeyboardBuilder:
+def create_customer_buttons(registration: bool)->InlineKeyboardBuilder:
     builder = InlineKeyboardBuilder()
     builder.row(InlineKeyboardButton(
         text="Назад",
@@ -61,11 +60,7 @@ def create_customer_buttons(registration: bool, link=None) -> InlineKeyboardBuil
         builder.row(InlineKeyboardButton(
             text="Новая заявка",
             callback_data="customer_newform")
-        )    
-        builder.row(InlineKeyboardButton(
-            text="Группа",
-            url=link)
-        )        
+        )      
     else:        
         builder.add(InlineKeyboardButton(
             text="Регистрация",
@@ -73,41 +68,96 @@ def create_customer_buttons(registration: bool, link=None) -> InlineKeyboardBuil
         ) 
     return builder
 
+def create_customer_send_form_buttons()->InlineKeyboardBuilder:
+    builder = InlineKeyboardBuilder()
+    builder.row(InlineKeyboardButton(
+        text="Да",
+        callback_data="form_send"
+    ))
+    builder.add(InlineKeyboardButton(
+        text="Нет",
+        callback_data="form_repeat"
+    ))
+    builder.row(InlineKeyboardButton(
+        text="Отмена",
+        callback_data="form_break"
+    ))
+    return builder 
 
-def admin_menu() -> InlineKeyboardMarkup:
-    buttons = [
-        [InlineKeyboardButton(text="Рассылка сообщений по пользователям", callback_data="notif")],
-        [InlineKeyboardButton(text="Изменить стартовое сообщение", callback_data="edit_start_mess")],
-        [InlineKeyboardButton(text="Просмотр Заказчиков", callback_data="view_customer")],
-        [InlineKeyboardButton(text="Просмотр Курьеров", callback_data="view_courier")],
-        [InlineKeyboardButton(text="Изменить стоимость подписки", callback_data="edit_amount")],
-        [InlineKeyboardButton(text="В меню", callback_data="menu")]
-    ]
-    keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
-    return keyboard
+async def create_choose_city_buttons(state:FSMContext)->InlineKeyboardBuilder:
+    data = await state.get_data()
+    n = data["n"]
+    n-=1
+    n*=6
+    city = data["city"]
+    builder = InlineKeyboardBuilder()
+    for i in range(6):
+        if n+i <= len(city)-1:
+            button = InlineKeyboardButton(
+                text=city[n+i],
+                callback_data=f"city_{n+i}")
+        else:
+            button = InlineKeyboardButton(
+                text="➖",
+                callback_data=f"none")
+        if (n+i)%3 == 0 or (n+i)%3==3:
+            builder.row(button)
+        else:
+            builder.add(button)
+    builder.row(InlineKeyboardButton(
+        text="<--",
+        callback_data=f"city_back")
+    )
+    builder.add(InlineKeyboardButton(
+        text="Отмена",
+        callback_data=f"city_break")
+    )
+    builder.add(InlineKeyboardButton(
+        text="-->",
+        callback_data=f"city_next")
+    )   
+    return builder
+  
+def status_work()->InlineKeyboardBuilder:
+    builder = InlineKeyboardBuilder()
+    builder.row(InlineKeyboardButton(
+        text="В работе",
+        callback_data="none"
+    ))
+    return builder
+ 
+def form_cancel(id:int)->InlineKeyboardBuilder:
+    builder = InlineKeyboardBuilder()
+    builder.row(InlineKeyboardButton(
+        text="Отменить заявку",
+        callback_data=f"formcancel_{id}"
+    ))
+    return builder
+          
+def customer_finish(id:int):
+    builder = InlineKeyboardBuilder()
+    builder.row(InlineKeyboardButton(
+        text="Завершить",
+        callback_data=f"finish_customer_{id}"
+    ))
+    return builder
 
-
-def confirmation(txt_y: str = "Да", txt_n: str = "Нет"):
-    buttons = [
-        [InlineKeyboardButton(text=txt_y, callback_data="yes")],
-        [InlineKeyboardButton(text=txt_n, callback_data="no")],
-        [InlineKeyboardButton(text="Отмена", callback_data="start")]
-    ]
-    keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
-    return keyboard
-
-
-def choice_people():
-    buttons = [
-        [InlineKeyboardButton(text="Курьеры", callback_data="notif_courier")],
-        [InlineKeyboardButton(text="Заказчики", callback_data="notif_customer")],
-        [InlineKeyboardButton(text="Все", callback_data="notif_all")]
-    ]
-    keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
-    return keyboard
-
-
-def cancel():
-    buttons = [[InlineKeyboardButton(text="Отмена", callback_data="start")]]
-    keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
-    return keyboard
+def courier_finish(id:int):
+    builder = InlineKeyboardBuilder()
+    builder.row(InlineKeyboardButton(
+        text="Отмена",
+        callback_data=f"finish_courier_{id}"
+    ))
+    return builder
+  
+def create_form_buttons(id:int):
+    builder = InlineKeyboardBuilder()
+    builder.row(InlineKeyboardButton(
+        text="Откликнуться",
+        callback_data=f"request_{id}"
+    ))
+    builder.row(InlineKeyboardButton(
+        text="Задать вопрос",
+        callback_data="request_chat"
+    ))
+    return builder
