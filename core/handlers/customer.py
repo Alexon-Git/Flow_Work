14617e0a9a1fce389731f276b161b9375bdaf6f7
Diohis <dimakulakov04mail.ru@gmail.com>
2024-02-10 13:56:41 +0700

    last commit

--- core/handlers/customer.py
+++ core/handlers/customer.py
@@ -199,7 +199,7 @@ async def customer_button_callback(callback: types.CallbackQuery,state: FSMConte
     else:
         await state.update_data(city = int(action))
         await state.set_state(CustomerRegistration.phone)
-        builder = create_contactI_button()
+        builder = create_contact_button()
         await callback.message.answer("Отправьте ваш номер телефона",reply_markup=builder.as_markup(resize_keyboard=True))
         await callback.message.edit_reply_markup(reply_markup=None)
         await callback.answer()
@@ -223,9 +223,11 @@ async def customer_contact(message:Message,state: FSMContext,bot:Bot):
     expire_date = datetime.datetime.now() + datetime.timedelta(days=1)
     chat_id = city_info[data["city"]]["chat id"]
     link = await bot.create_chat_invite_link(chat_id=chat_id, expire_date= int( expire_date.timestamp()),member_limit= 1)
-    msg = await message.answer(f"Регистрация успешно завершена. Теперь вы можете создавать свои заявки в стартовом меню или по кнопке ниже.\n➖➖➖➖➖➖➖➖➖➖➖➖➖\nСсылка на вступление в группу города: {link.invite_link}",reply_markup=ReplyKeyboardRemove())
+    msg = await message.answer("delete",reply_markup=ReplyKeyboardRemove())
+    await msg.delete()
     builder = create_newform_button()
-    await bot.edit_message_reply_markup(chat_id=msg.chat.id,message_id=msg.message_id,reply_markup=builder.as_markup())
+    await message.answer(f"Регистрация успешно завершена. Теперь вы можете создавать свои заявки в стартовом меню или по кнопке ниже.\n➖➖➖➖➖➖➖➖➖➖➖➖➖\nСсылка на вступление в группу города: {link.invite_link}",reply_markup=builder.as_markup())
+    #await bot.edit_message_reply_markup(chat_id=msg.chat.id,message_id=msg.message_id,reply_markup=builder.as_markup())
     new_customer = {
         "username" : message.from_user.username,
         "user_id":message.from_user.id,
--- core/keyboards/inline.py
+++ core/keyboards/inline.py
@@ -82,10 +82,6 @@ def create_newform_button()->InlineKeyboardBuilder:
     )
     return builder
 
-def create_contactI_button()->InlineKeyboardBuilder:
-    builder = InlineKeyboardBuilder()
-    builder.add(InlineKeyboardButton(text="Поделиться номером", callback_data="contact", request_contact=True))
-    return builder
 def create_customer_send_form_buttons() -> InlineKeyboardBuilder:
     builder = InlineKeyboardBuilder()
     builder.row(InlineKeyboardButton(
